b4da9467e296cc7edd55803d4dfacbc2d963ee16
Nima <88039431+n-ii-ma@users.noreply.github.com>
2021-12-05 15:50:20 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import App from "./app/App";
 import store from "./app/store";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
